d44deb0dc5496bcba4243cfdb913b432a5e23502
Jake <Greenwood.H.Jake@gmail.com>
2025-06-09 15:28:36 +0100

    Add

---
+++ src/main.c
@@ -0,0 +1,79 @@
+#include <SDL2\SDL.h>
+#include <stdio.h>
+
+#define WIDTH 900
+#define HEIGHT 600
+#define LENGTH 50
+
+int main(int argc, char* argv[]) {
+    //printf("hello world");
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("The window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
+    SDL_Surface* surface = SDL_GetWindowSurface(window);
+    SDL_Rect rect = {200,200,LENGTH,LENGTH};
+    SDL_Event event;
+
+    float dx = 1;
+    float dy = 1;
+
+    int game = 1;
+    while (game == 1) {
+        SDL_FillRect(surface, NULL, 0x00000000);
+
+        while(SDL_PollEvent(&event)){
+            switch (event.type){
+                case(SDL_QUIT):
+                game=0;
+                break;
+            }
+        };
+
+        if ((float)rect.x + (float)LENGTH + dx > (float)WIDTH) {
+            dx /= abs(dx);
+            dy /= abs(dy);
+            dx *= -1;
+        }
+        if ((float)rect.x + dx < 0.0) {
+            dx /= abs(dx);
+            dy /= abs(dy);
+            dx *= -1;
+        }
+        if ((float)rect.y + (float)LENGTH + dy > (float)HEIGHT) {
+            dx /= abs(dx);
+            dy /= abs(dy);
+            dy *= -1;
+        }
+        if ((float)rect.y +  dy < 0.0) {
+            dx /= abs(dx);
+            dy /= abs(dy);
+            dy *= -1;
+        }
+        rect.x += (int)dx;
+        rect.y += (int)dy;
+
+        dx+= dx*0.01;
+        dy+= dy*0.01;
+        
+        /*
+        if (dx < 0)
+            dx -=1;
+        else
+            dx +=1;
+
+        if (dy < 0)
+            dy -=1;
+        else
+            dy +=1;
+        */
+        SDL_FillRect(surface, &rect, 0xffffffff);
+        SDL_UpdateWindowSurface(window);
+
+        SDL_Delay(16);
+    };
+
+    SDL_DestroyWindowSurface(window);
+    SDL_DestroyWindow(window);
+    
+    return 0;
+}
