b6dc0a735641a9a9d54d025172fa9934e8a570db
Jake <Greenwood.H.Jake@gmail.com>
2025-06-11 15:43:29 +0100

    removed excess

--- src/main.c
+++ src/main.c
@@ -66,21 +66,6 @@ int main(int argc, char* argv[]) {
         rect.x = (int)ball.x;
         rect.y = (int)ball.y;
 
-        //dx+= dx*0.01;
-        //dy+= dy*0.01;
-        
-        /*
-        if (dx < 0)
-            dx -=1;
-        else
-            dx +=1;
-
-        if (dy < 0)
-            dy -=1;
-        else
-            dy +=1;
-        */
-
         SDL_FillRect(surface, &rect, 0xffffffff);
         SDL_UpdateWindowSurface(window);
     };
